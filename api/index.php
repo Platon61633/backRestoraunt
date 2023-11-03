b79a021462901d23664e9555b92b1068555c434c
Platon61633 <peredreevplaton@gmail.com>
2023-11-03 11:26:20 +0300

    send 2

--- api/index.php
+++ api/index.php
@@ -170,6 +170,8 @@ switch ($_GET['for']) {
                     if (!mysqli_query($connect, "INSERT INTO `reserved` (`id`, `name`, `surname`, `date`, `time`, `tel`, `kolvo`, `info`) 
                     VALUES (NULL, '$arr[0]', '$arr[1]', '$arr[2]', '$arr[3]', '$arr[4]', '$arr[5]', '$arr[6]')")) {
                         echo 1;
+                    }else {
+                        echo 2;
                     }
                     break;
                 
